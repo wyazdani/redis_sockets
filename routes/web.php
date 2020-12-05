0fab4ab0fd3b99dc3ef46fd3257215887cb45e6a
Waqar <waqaryazdani2@gmail.com>
2020-12-05 21:12:15 +0500

    Incrementing REDIS

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,12 @@ Route::get('/', function () {
 
     return view('welcome')->withVisits($visits);
 });
+Route::get('articles/trending', function () {
+
+    $trending   =   Redis::zrevrange('trending_articles',0,2);
+
+    dd($trending);
+});
 Route::get('videos/{id}', function ($id) {
 
     $downloads = Redis::get('videos.'.$id.'.downloads');
@@ -30,4 +36,13 @@ Route::get('videos/{id}/download', function ($id) {
     Redis::incr('videos.'.$id.'.downloads');
 
     return back();
+});
+
+Route::get('articles/{article}', function (\App\Models\Article $article) {
+
+    Redis::zincrby('trending_articles',1,$article->id);
+
+    Redis::zremrangebyrank('trending_articles',0,-4);
+
+    return $article;
 });
