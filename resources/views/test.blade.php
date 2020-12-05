d2b9bb04d6775f1204d75b760b6b87926e9be6ce
Waqar <waqaryazdani2@gmail.com>
2020-12-05 17:31:45 +0500

    First Commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<body class="antialiased">
+    <h1>Number of Downloads</h1>
+    <p><b>{!! $downloads !!}</b></p>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $visits = Redis::incr('visits');
+
+    return view('welcome')->withVisits($visits);
+});
+Route::get('videos/{id}', function ($id) {
+
+    $downloads = Redis::get('videos.'.$id.'.downloads');
+
+    return view('test')->withDownloads($downloads);
+});
+Route::get('videos/{id}/download', function ($id) {
+
+    Redis::incr('videos.'.$id.'.downloads');
+
+    return back();
+});
